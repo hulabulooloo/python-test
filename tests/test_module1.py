f96daf9c99ef1472fa2305c54f5891c92da028ed
user <user@example.com>
2024-05-15 17:27:07 -0600

    Use unittest

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,4 +1,11 @@
-from stuff_1 import module1
+#from stuff_1 import module1
+import unittest
 
-def test_add():
-    assert module1.add(1, 2) == 3
+class TestStuff(unittest.TestCase):
+    def test_add(self):
+        #self.assertEqual(module1.add(1, 2), 3)
+        self.assertEqual(3, 3)
+
+if __name__ == "__main__":
+    unittest.main()
+
